ab876101a3e9d0649b784893882d21c7acafac34
zaccoding <zaccoding725@gmail.com>
2020-04-05 11:23:04 +0900

    go full stack: ~ch04

---
+++ fullstack/README.md
@@ -0,0 +1,7 @@
+# Hands On Full Stack Development with-Go  
+
+- [Go 풀스택 웹 개발](https://book.naver.com/bookdb/book_detail.nhn?bid=16298674)  
+- [Source from github](https://github.com/PacktPublishing/Hands-On-Full-Stack-Development-with-Go)  
+
+
+ 
--- workspace/basic/main.go
+++ workspace/basic/main.go
@@ -1,12 +1,66 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 type MyStruct struct {
 	age int
 }
 
 func main() {
+	// panicTest()
+	regularChannelTest()
+}
+
+func regularChannelTest() {
+	myChannel := make(chan int)
+	// myBufferedChannel := make(chan int, 10) // 10개의 int 버퍼 채널
+	go runLoopSend(10, myChannel)
+	go runLoopReceive(myChannel)
+
+	time.Sleep(2 * time.Second)
+}
+
+func runLoopSend(n int, ch chan int) {
+	for i := 0; i < n; i++ {
+		ch <- i
+	}
+	close(ch)
+}
+
+func runLoopReceive(ch chan int) {
+	for {
+		// 채널이 열려 있으면 ok == true, 닫혀 있으면 ok == false
+		i, ok := <-ch
+		if !ok {
+			break
+		}
+		fmt.Println("Receive value:", i)
+	}
+
+	//for i := range ch {
+	//	fmt.Println("Receive value:", i)
+	//}
+}
+
+func panicTest() {
+	forcePanic(true)
+	fmt.Println("Hello world")
+}
+
+func checkPanic() {
+	if r := recover(); r != nil {
+		fmt.Println("A Panic was captured, message :", r)
+	}
+}
+
+func forcePanic(p bool) {
+	defer checkPanic()
+	if p {
+		panic("panic requested")
+	}
 }
 
 func callByTest() {
